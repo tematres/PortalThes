ae4b2b7daac3f1d1387cf2c497f01d7d852a8238
tematres <tematres@r020.com.ar>
2018-10-03 09:26:31 -0300

    add pt-BR lang to classiffy. Thanks to Alison Silva from Tibunal de Contas

---
+++ apps/classify/lang/pt_BR.php
@@ -0,0 +1,26 @@
+<?php
+#   TemaTres : aplicación para la gestión de lenguajes documentales #       #
+#                                                                        #
+#   Distribuido bajo Licencia GNU Public License, versión 2 (de junio de 1.991) Free Software Foundation
+#   #
+###############################################################################################################
+#
+
+/*TERMS_DISTILLER PLUGIN */
+setlocale(LC_ALL, 'pt_BR');
+define('CLASSIFY_SERVICE_title','Extrator de Palavras-chave');
+define('CLASSIFY_SERVICE_help','Insira aqui um texto que deseja analisar em busca de palavras-chave...');
+define('CLASSIFY_SERVICE_terms_tag','Exclua os termos que considerar incorretos. Pode editar ou adicionar termos. Utilize no máximo %s termos.');
+define('CLASSIFY_SERVICE_description','Esta ferramenta permite analisar um breve texto e identificar <strong> potenciais palavras-chave</strong> utilizando o ');
+define('LABELFORM_evalTerms','Analisar termos');
+define('LABELFORM_evalTerms','Analisar termos');
+define('CLASSIFY_SERVICE_backLink','voltar');
+define('CLASSIFY_SERVICE_placeholderAddKeyWords','exclua, edite ou adicione termos');
+define('CLASSIFY_SERVICE_placeholderAddKeyWords2','não foi possível adicionar palavras-chave...');
+define('CLASSIFY_SERVICE_copyKeywords','copiar palavras-chave');
+define('CLASSIFY_SERVICE_helpStep1','Insira um breve texto para análise (1 a 2 parágrafos).');
+//define('CLASSIFY_SERVICE_helpStep2','<strong>Selecione</strong>, <strong>edite</strong> ou <strong>adicione</strong> termos que serão avaliados pelo vocabulário controlado.');
+define('CLASSIFY_SERVICE_helpStep2','<strong>Selecione os termos</strong> que melhor representam o conteúdo.');
+define('CLASSIFY_SERVICE_helpStep3','<strong>Copie</strong> os termos para área de transferência utizando o <strong><span style="color:green">botão verde</span></strong>.');
+define('CLASSIFY_SERVICE_helpStep4','<!--<strong>Copie</strong> os termos para área de transferência utizando o <strong><span style="color:green">botão verde</span></strong>.-->');
+?>
